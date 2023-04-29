97398299af98eaf08c08fbf8ec6edce14d0f327b
0xCAF2 <pub@0xcaf2.dev>
2023-04-29 16:33:49 +0900

    introduce a Flutter app in iframe

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,20 @@ import Blockly from 'blockly'
 // @ts-ignore
 import DarkTheme from '@blockly/theme-dark'
 
-const divBlockly = ref()
+const divBlockly = ref<HTMLDivElement>()
+const iframeAppDisplay = ref<HTMLIFrameElement>()
 
+// @ts-ignore
 let editableWorkspace: Blockly.Workspace
 
+declare global {
+  interface Window {
+    hello: () => void
+  }
+}
+
 onMounted(() => {
-  editableWorkspace = Blockly.inject(divBlockly.value, {
+  editableWorkspace = Blockly.inject(divBlockly.value!, {
     renderer: 'zelos',
     sounds: false,
     theme: DarkTheme,
@@ -30,17 +38,25 @@ onMounted(() => {
       ]
     }
   })
+
+  iframeAppDisplay.value!.contentWindow!.hello = () => console.log('Hello, World.')
 })
 
 </script>
 
 <template>
   <div ref="divBlockly" id="divBlockly"></div>
+  <iframe ref="iframeAppDisplay" id="iframeAppDisplay" src="/apps/intro/build/web/index.html"></iframe>
 </template>
 
 <style scoped>
 #divBlockly {
   width: 640px;
   height: 400px;
+}
+
+#iframeAppDisplay {
+  width: 360px;
+  height: 640px;
 }
 </style>
